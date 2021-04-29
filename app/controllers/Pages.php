93cbf25f87910e5d77522a2afe7cf626e4a7ac05
SaifN97 <saifnarpali@gmail.com>
2021-04-29 23:20:15 +0530

    application core

---
+++ app/controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pages
+{
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        echo "Home Page";
+    }
+
+    public function about()
+    {
+        echo "About";
+    }
+}
